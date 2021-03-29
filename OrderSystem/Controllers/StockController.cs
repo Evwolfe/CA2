55986e8e0f5248b927b915d08b7f2a12e4804018
StephenWard626 <78171486+StephenWard626@users.noreply.github.com>
2021-03-29 12:07:19 +0100

    StockContrller changes and a View Added

--- OrderSystem/Controllers/StockController.cs
+++ OrderSystem/Controllers/StockController.cs
@@ -10,18 +10,20 @@ namespace OrderSystem.Controllers
 {
     public class StockController : Controller
     {
-        private readonly StockContext _context;
-
-        public StockController()
-        {
-            _context = new StockContext();
-            _context.Database.EnsureCreated();
-        }
+        private static List<StockDetails> stock
+             = new List<StockDetails>()
+             {
+                new StockDetails() { ProductID = "CB001", Name = "Cadburys Dairy Milk", Price = 5.99, Qty = 50 },
+                new StockDetails() { ProductID = "CB002", Name = "Cadburys Mint Crisp", Price = 4.99, Qty = 40 },
+                new StockDetails() { ProductID = "CB003", Name = "Cadburys Wholenut", Price = 6.99, Qty = 50 },
+                new StockDetails() { ProductID = "CB004", Name = "Cadburys Caramel", Price = 5.00, Qty = 30 }
+             };
 
         // GET: StockController
         public ActionResult Index()
         {
-            return View();
+            //ViewBag.TotalPrice = string.Format(cart.CalcTotal().ToString("C2"));
+            return View(stock);
         }
 
         // GET: StockController/Details/5
@@ -92,5 +94,20 @@ namespace OrderSystem.Controllers
                 return View();
             }
         }
+
+        /*private void SeedDB()
+        {
+            StockDetails s1 = new StockDetails() { ProductID = "CB001", Name = "Cadburys Dairy Milk", Price = 5.99, Qty = 50 };
+            StockDetails s2 = new StockDetails() { ProductID = "CB002", Name = "Cadburys Mint Crisp", Price = 4.99, Qty = 40 };
+            StockDetails s3 = new StockDetails() { ProductID = "CB003", Name = "Cadburys Wholenut", Price = 6.99, Qty = 50 };
+            StockDetails s4 = new StockDetails() { ProductID = "CB004", Name = "Cadburys Caramel", Price = 5.00, Qty = 30 };
+
+            Stock.Add(s1);
+            Stock.Add(s2);
+            Stock.Add(s3);
+            Stock.Add(s4);
+
+            Stock.SaveChanges();
+        }*/
     }
 }
