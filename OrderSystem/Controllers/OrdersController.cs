bbdd1cb82384ff5a056d4beba62c9ca8e39297dc
StephenWard626 <78171486+StephenWard626@users.noreply.github.com>
2021-05-02 16:24:14 +0100

    Some LAyout updates

--- OrderSystem/Controllers/OrdersController.cs
+++ OrderSystem/Controllers/OrdersController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using CA2_OrderSystem.Models;
+using CA2_OrderSystem.Controllers;
 
 namespace OrderSystem.Controllers
 {
@@ -54,6 +55,8 @@ namespace OrderSystem.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
+
+        /*
         public async Task<IActionResult> Create([Bind("ID,OrderID,Address,Details")] Orders orders)
         {
             if (ModelState.IsValid)
@@ -64,6 +67,42 @@ namespace OrderSystem.Controllers
             }
             return View(orders);
         }
+        */
+
+        public async Task<IActionResult> Create(Orders orders)
+        {
+            if (ModelState.IsValid)
+            {
+
+                List<string> s1 = new List<string>();
+                List<CartItems> c1 = CartController.c1.ReturnCart();
+                foreach (CartItems xx in c1)
+                {
+                    s1.Add(xx.Name);
+                }
+                string s2 = string.Join(", ", s1);
+
+                int ordID;
+                if (_context.OrderDetails.Count() == 0)
+                {
+                    ordID = 1;
+                }
+                else
+                {
+                    ordID = _context.OrderDetails.Count() + 1;
+                }
+
+                _context.Add(new Orders { OrderID = ordID, Address = orders.Address, Details = s2 });
+
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(orders);
+        }
+
+
+
+
 
         // GET: Orders/Edit/5
         public async Task<IActionResult> Edit(int? id)
