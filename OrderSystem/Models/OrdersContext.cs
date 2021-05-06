d08c0a4fb3e8c234c8241fc3bcf082f76bf4c0a0
StephenWard626 <78171486+StephenWard626@users.noreply.github.com>
2021-05-06 10:07:55 +0100

    Added Azure Connection Strings

--- OrderSystem/Models/OrdersContext.cs
+++ OrderSystem/Models/OrdersContext.cs
@@ -8,9 +8,9 @@ namespace CA2_OrderSystem.Models
 {
     public class OrdersContext : DbContext
     {
-        private const string connectionString =
-           "Server=(localdb)\\mssqllocaldb;DataBase=Orders6;Trusted_Connection=True;";
+        //private const string connectionString ="Server=(localdb)\\mssqllocaldb;DataBase=Orders6;Trusted_Connection=True;";
 
+        private const string connectionString = "Server=tcp:stephenca2.database.windows.net,1433;Initial Catalog=Orders;Persist Security Info=False;User ID=ad;Password=Kingdom2Hearts;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
 
         public DbSet<Orders> OrderDetails { get; set; }
 
--- OrderSystem/Models/StockContext.cs
+++ OrderSystem/Models/StockContext.cs
@@ -11,11 +11,12 @@ namespace CA2_OrderSystem.Models
     {
         public DbSet<Stock> Stock { get; set; }
 
-        private const string connectionString =
-            "Server=(localdb)\\mssqllocaldb;DataBase=Stock1;Trusted_Connection=False;";
+        //private const string connectionString = "Server=(localdb)\\mssqllocaldb;DataBase=Stock1;Trusted_Connection=False;";
 
-        //The below tells the system to use the connection string we entered above - directing it to the db
-        protected override void OnConfiguring(DbContextOptionsBuilder optionBuilder)
+        private const string connectionString = "Server=tcp:stephenca2.database.windows.net,1433;Initial Catalog=StockSystem;Persist Security Info=False;User ID=ad;Password=Kingdom2Hearts;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+
+    //The below tells the system to use the connection string we entered above - directing it to the db
+    protected override void OnConfiguring(DbContextOptionsBuilder optionBuilder)
         {
             optionBuilder.UseSqlServer(connectionString);
         }
